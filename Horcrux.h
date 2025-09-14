c635d8295f0006f32258ce6ce87bc253ca9fdce6
thaGnonsis <81764173+tr1llkilla@users.noreply.github.com>
2025-09-14 15:17:36 -0500

    Create Horcrux.h

---
+++ Horcrux.h
@@ -0,0 +1,13 @@
+Copyright © 2025 Cadell Richard Anderson
+
+//Horcrux.h
+
+#pragma once
+#include "ElevenValueBoolean.h"
+
+namespace Horcrux {
+    // Corrected: Removed `inline` and added `extern` for compatibility.
+    extern ElevenValueBoolean Memory;
+    extern ElevenValueBoolean Compiler;
+    extern ElevenValueBoolean IO;
+}
